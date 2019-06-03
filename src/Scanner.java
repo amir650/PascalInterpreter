b6a295ca83da6deabefe88293fe392bc05ff8e27
amir.afghani <amir650@gmail.com>
2019-06-02 20:05:45 -0700

    Setting up framework for the interpreter

--- src/Scanner.java
+++ src/Scanner.java
@@ -1,2 +1,27 @@
-public class Scanner {
+public abstract class Scanner {
+
+    protected Source source;
+    private Token currentToken;
+
+    public Scanner(Source source) {
+        this.source = source;
+    }
+
+    public Token currentToken() {
+        return this.currentToken;
+    }
+
+    public Token nextToken() throws Exception {
+        return null;
+    }
+
+    protected abstract Token extractToken() throws Exception;
+
+    public char currentChar() throws Exception {
+        return this.source.currentCharacter();
+    }
+
+    public char nextChar() throws Exception {
+        return this.source.nextCharacter();
+    }
 }
